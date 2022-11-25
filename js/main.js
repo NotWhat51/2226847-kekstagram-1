705ebbb6f0e65563876570090d7673443f4dadb9
NotWhat51 <sofiabelsk51@gmail.com>
2022-11-25 15:36:58 +0300

    Draw me completely (part 2)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,35 @@
-import { createPublication } from "./data.js";
+import { setOfPublication } from "./data.js";
 import { renderPicture} from "./render.js";
+import { openBigPicture, bigPicture } from "./big_view.js";
 
-const posts = createPublication();
-renderPicture(posts);
+const posts = setOfPublication();
+
+for (const post of posts) {
+    renderPicture(post);
+}
+
+const pictures = document.querySelector('.pictures');
+
+for (const post of posts) {
+    const picture= pictures.querySelector(`img[src="${post.url}"]`).parentNode;
+
+    picture.addEventListener('click', (evt) => {
+        evt.preventDefault();
+        document.body.classList.add('modal-open');
+        openBigPicture(post);
+    })
+}
+
+const cancel = bigPicture.querySelector('.big-picture__cancel');
+
+cancel.addEventListener('click', () => {
+    bigPicture.classList.add('hidden');
+    document.body.classList.remove('modal-open');
+})
+
+document.addEventListener('keydown', (evt) => {
+    if (evt.key === 'Escape') {
+        bigPicture.classList.add('hidden');
+        document.body.classList.remove('modal-open');
+    }
+})
